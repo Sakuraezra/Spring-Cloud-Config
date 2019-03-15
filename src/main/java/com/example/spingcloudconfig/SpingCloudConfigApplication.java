57ab1a0d8493b61f73de656fcc0f4b5abeace583
475815538@qq.com <475815538@qq.com>
2019-03-15 11:37:54 +0800

    文件夹

---
+++ src/main/java/com/example/spingcloudconfig/SpingCloudConfigApplication.java
@@ -0,0 +1,13 @@
+package com.example.spingcloudconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingCloudConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingCloudConfigApplication.class, args);
+	}
+
+}
